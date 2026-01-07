a032a41435cdee8e77e4bb479510993e177a2bb5
Sergio <nimbite@hotmail.com>
2026-01-07 14:11:26 +0100

    Commit mostrar Estado

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.util.Scanner;
 public class Main {
+    static int saciedad=6, energia=6, diversion = 6;
     public static void main() {
         // DECLARACION DE VARIABLES
         int opcion = 0;
@@ -42,6 +43,43 @@ public class Main {
         opcion = lectura.nextInt();
 
         return opcion;
+    }
+
+    private static void mostrarEstado(){
+        // ESTADO CONTENTO
+        if (saciedad > 5 && energia > 5 && diversion > 5) {
+            System.out.println("(•‿•)  ¡Estoy feliz! \n" +
+                               "/|_|\\ \n" +
+                                "| | \n");
+        }
+
+        // ESTADO TRISTE
+        if (diversion < 5){
+            System.out.println("(-︵-)  Estoy aburrido \n" +
+                               "/|_|\\  ¡Juega conmigo! \n" +
+                                "/ \\ \n");
+        }
+
+        // ESTADO CANSADO
+        if (energia < 5){
+            System.out.println("(-_-)   Zzz \n" +
+                               "/|_|\\ \n" +
+                                "| | \n");
+        }
+
+        // ESTADO HAMBRIENTO
+        if (saciedad < 5){
+            System.out.println("(•︵•)   ¡Tengo hambre! \n" +
+                               "/|x|\\ \n" +
+                                "| | \n");
+        }
+
+        // SI ALGUN ESTADO LLEGA A 0, FIN DEL JUEGO
+        if (saciedad == 0 || energia == 0 || diversion == 5){
+            System.out.println("(x_x)  :( Game over ): \n" +
+                               "/|_|\\ \n" +
+                                "| | \n");
+        }
     }
 }
 
